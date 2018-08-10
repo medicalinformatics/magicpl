8aa41c8949a95f551f9670d2e13b6177e7e66956
Andreas Borg <borga@uni-mainz.de>
2018-08-10 16:49:59 +0200

    Code cleanup, Javadoc

--- src/main/java/de/mainzelliste/paths/backend/Authenticator.java
+++ src/main/java/de/mainzelliste/paths/backend/Authenticator.java
@@ -1,10 +1,7 @@
 package de.mainzelliste.paths.backend;
 
-import java.security.PublicKey;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,9 +11,10 @@ import javax.ws.rs.core.Response.Status;
 
 import de.mainzelliste.paths.configuration.AnonymousClient;
 import de.mainzelliste.paths.configuration.Authentication;
-//import de.pseudonymisierung.mainzelliste.SubnetUtils;
-//import de.pseudonymisierung.mainzelliste.Servers.Server;
 
+/**
+ * Utility class for access control (authentication, authorization).
+ */
 public class Authenticator {
 	/**
 	 * Represents one registered client.
@@ -33,11 +31,17 @@ public class Authenticator {
 	private HashMap<String, Client> clients = new HashMap<>();
 	private Client anonymousClient = new Client();
 
+	/**
+	 * Create instance from given configuration.
+	 * 
+	 * @param config
+	 *            Configuration (typically de-serialized from XML).
+	 */
 	public Authenticator(Authentication config) {
 		for (de.mainzelliste.paths.configuration.Client clientConfig : config.getClient()) {
 			Client thisClient = new Client();
 			thisClient.apiKey = clientConfig.getApiKey();
-			thisClient.permissions = new HashSet(clientConfig.getPermissions());
+			thisClient.permissions = new HashSet<>(clientConfig.getPermissions());
 			thisClient.allPermissions = (clientConfig.getAllPermissions() != null);
 			clients.put(clientConfig.getApiKey(), thisClient);
 		}
@@ -53,7 +57,8 @@ public class Authenticator {
 	/**
 	 * Check whether a client is authorized for a request according to the
 	 * authentication/authorization configuration. If anonymous access to the
-	 * requested action (permission) is configured, no actual authentication occurs.
+	 * requested action (permission) is configured, no actual authentication
+	 * occurs.
 	 *
 	 * If access is denied, an appropriate WebApplicationException is thrown.
 	 *
@@ -63,8 +68,10 @@ public class Authenticator {
 	 *            The permission to check (name of a path).
 	 */
 	public void checkPermission(HttpServletRequest req, String permission) {
-		// First, check if anonymous access is possible for the requested permission
-		if (this.anonymousClient.allPermissions || (this.anonymousClient.permissions != null && this.anonymousClient.permissions.contains(permission)))
+		// First, check if anonymous access is possible for the requested
+		// permission
+		if (this.anonymousClient.allPermissions
+				|| (this.anonymousClient.permissions != null && this.anonymousClient.permissions.contains(permission)))
 			return;
 
 		@SuppressWarnings("unchecked")
@@ -75,46 +82,17 @@ public class Authenticator {
 			Client client = clients.get(apiKey);
 
 			if (client == null) {
-				// logger.info("No server found with provided API key " + apiKey);
 				throw new WebApplicationException(Response.status(Status.UNAUTHORIZED)
 						.entity("Please supply your API key in HTTP header field 'apiKey'.").build());
 			}
 
-			// if(!clients.allowedRemoteAdresses.contains(req.getRemoteAddr())){
-			// boolean addressInRange = false;
-			// for (SubnetUtils thisAddressRange : server.allowedRemoteAdressRanges) {
-			// try {
-			// if (thisAddressRange.getInfo().isInRange(req.getRemoteAddr())) {
-			// addressInRange = true;
-			// break;
-			// }
-			// } catch (IllegalArgumentException e) {
-			// // Occurs if an IPv6 address was transmitted
-			// logger.error("Could not parse IP address " + req.getRemoteAddr(), e);
-			// break;
-			// }
-			// }
-			// if (!addressInRange) {
-			// logger.info("IP address " + req.getRemoteAddr() + " rejected");
-			// throw new WebApplicationException(Response
-			// .status(Status.UNAUTHORIZED)
-			// .entity(String.format("Rejecting your IP address %s.", req.getRemoteAddr()))
-			// .build());
-			// }
-			// }
-
 			perms = client.permissions;
 			req.getSession().setAttribute("permissions", perms);
-			// logger.info("Server " + req.getRemoteHost() + " logged in with permissions "
-			// + Arrays.toString(perms.toArray()) + ".");
 		}
 
 		if (!perms.contains(permission)) { // Check permission
-			// logger.info("Access from " + req.getRemoteHost() + " is denied since they
-			// lack permission " + permission + ".");
 			throw new WebApplicationException(Response.status(Status.UNAUTHORIZED)
 					.entity("Your permissions do not allow the requested access.").build());
 		}
 	}
-
 }
